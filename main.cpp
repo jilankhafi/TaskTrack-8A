29f619fc59ed2d846673a51e79dbc6368cfc19dd
jilankhafi <jilankhafi@gmail.com>
2025-11-20 16:51:14 +0700

    Debugging Function Main

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main() {
     string pilihan;
+    string menu[6] = {"1", "2", "3", "4", "5", "6"};
     while (true) {
         cout << "\n===== MENU TUGAS =====\n";
         cout << "1. Tambah Tugas\n";
@@ -23,29 +24,28 @@ int main() {
 
         cin >> pilihan;
 
-        if (pilihan == "1") {
-            int status = tambahTugas();
-            if (status == 1)
+        if (pilihan == menu[0]) {
+            if (tambahTugas() == 1)
                 cout << "Tugas berhasil disimpan!\n";
         }
-        else if (pilihan == "2") {
+        else if (pilihan == menu[1]) {
             lihatTugas();
         }
-        else if (pilihan == "3") {
+        else if (pilihan == menu[2]) {
             hapusSemuaTugas();
         }
-        else if (pilihan == "4") {
+        else if (pilihan == menu[3]) {
             cariTugas();
         }
-        else if (pilihan == "5") {
+        else if (pilihan == menu[4]) {
             cout << "Total tugas: " << hitungTotalTugas() << endl;
         }
-        else if (pilihan == "6") {
+        else if (pilihan == menu[5]) {
             cout << "Selamat Tinggal!\n";
             break;
         }
         else {
-            cout << "Menu tidak valid! Harap masukkan 1 - 6.\n";
+            cout << "Menu tidak valid! Masukkan angka 1 - 6.\n";
         }
     }
 
